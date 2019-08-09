8b9b44290dada16c178a5b0293ead4912af6036e
Enrico Ros <enrico.ros@gmail.com>
2019-08-08 21:27:32 -0700

    Generate South Park-like conversation lines

--- src/interactive_conditional_samples.py
+++ src/interactive_conditional_samples.py
@@ -1,12 +1,51 @@
-#!/usr/bin/env python3
-
+##!/usr/bin/env python3
 import fire
 import json
 import os
 import numpy as np
 import tensorflow as tf
+import time
 
 import model, sample, encoder
+
+# disable the TF warning messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# disable the TF deprecation messages
+tf._deprecation_wrapper._PER_MODULE_WARNING_LIMIT = 0
+# enable XLA - doesn't give performance advantage
+# os.environ['TF_XLA_FLAGS'] = '--tf_xla_cpu_global_jit'
+
+
+SOUTH_LINES = {}
+def parse_sp_episodes(fileName):
+    with open(fileName, newline='') as csvfile:
+        import csv
+        for row in csv.reader(csvfile):
+            if len(row) == 1 or row[0] == 'Season':
+                continue
+            assert len(row) == 4
+            season = int(row[0])
+            episode = int(row[1])
+            character = row[2]
+            line = row[3]
+            SOUTH_LINES.setdefault(season, {}).setdefault(episode, []).append([character, line])
+
+def make_sp_text(lines, season, episode, skip, count, extra):
+    transcripts = lines[season][episode]
+    text = ""
+    for line in transcripts:
+        skip = skip - 1
+        if skip >= 0:
+            continue
+        text += line[0] + ": " + line[1] + extra
+        count = count - 1
+        if count < 1:
+            break
+    return text
+
+parse_sp_episodes('Season-18.csv')
+parse_sp_episodes('Season-19.csv')
+
 
 def interact_model(
     model_name='124M',
@@ -38,6 +77,21 @@ def interact_model(
      :models_dir : path to parent folder containing model subfolders
      (i.e. contains the <model_name> folder)
     """
+
+    # Robert Miles - YouTube comments config
+    nsamples = 1
+    length = 150
+    top_k = 40
+
+    # Enrico's
+    # nsamples = 4
+    # batch_size = 4
+    nsamples = 2
+    batch_size = 2
+    length = 100
+    top_k = 40
+
+
     models_dir = os.path.expanduser(os.path.expandvars(models_dir))
     if batch_size is None:
         batch_size = 1
@@ -53,10 +107,10 @@ def interact_model(
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
-    with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
+    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
-        tf.set_random_seed(seed)
+        tf.compat.v1.set_random_seed(seed)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
@@ -64,7 +118,7 @@ def interact_model(
             temperature=temperature, top_k=top_k
         )
 
-        saver = tf.train.Saver()
+        saver = tf.compat.v1.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
@@ -73,18 +127,28 @@ def interact_model(
             while not raw_text:
                 print('Prompt should not be empty!')
                 raw_text = input("Model prompt >>> ")
+            if raw_text == "sp":
+                txt_season = 18
+                txt_episode = 1
+                print(" Using South Park season " + str(txt_season) + " ep " + str(txt_episode) + " !!")
+                raw_text = make_sp_text(SOUTH_LINES, txt_season, txt_episode, 135, 30, "\n")
+                raw_text += "Cartman: "
+                print(raw_text)
+            print("got it.. processing")
+            start_time = time.time()
             context_tokens = enc.encode(raw_text)
             generated = 0
             for _ in range(nsamples // batch_size):
                 out = sess.run(output, feed_dict={
                     context: [context_tokens for _ in range(batch_size)]
                 })[:, len(context_tokens):]
+                print("done. Samples below:")
                 for i in range(batch_size):
                     generated += 1
                     text = enc.decode(out[i])
                     print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
-            print("=" * 80)
+                    print("Cartman: " + text)
+            print("=" * 80 + ", Elapsed: " + str(time.time() - start_time))
 
 if __name__ == '__main__':
     fire.Fire(interact_model)
