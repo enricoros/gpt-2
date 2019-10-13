6c324857bb697042c5c11380b6d0ff0ca13e0ac3
Enrico Ros <enrico.ros@gmail.com>
2019-10-12 17:43:11 -0700

    Fix

--- src/flasky.py
+++ src/flasky.py
@@ -97,7 +97,7 @@ def run_app():
                 "input": in_text,
                 "samples": in_samples,
                 "completions": output_texts,
-                "contexts": np.array2string(output_contexts),
+                "contexts": np.array_str(output_contexts),
                 "backend_elapsed": time.time() - initial_call_time
             }
             return json.dumps(response), 200
