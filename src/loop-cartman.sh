671654b4279db111e603843286f234915ef81961
Enrico Ros <enrico.ros@gmail.com>
2019-11-17 13:11:14 -0800

    Fredliu-345M: 3 samples x 70.

--- src/loop-cartman.sh
+++ src/loop-cartman.sh
@@ -3,5 +3,5 @@ source ../venv-tf15/Scripts/activate
 while :
 do
   echo "Press [CTRL+C] to stop.."
-  python flasky.py --http_host=0.0.0.0 --http_port=1303 --model_name=345M-Cartman-Fredliu1 --sample_size=4 --length=60
+  python flasky.py --http_host=0.0.0.0 --http_port=1303 --model_name=345M-Cartman-Fredliu1 --sample_size=3 --length=70
 done
