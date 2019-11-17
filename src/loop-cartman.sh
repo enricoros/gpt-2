6c08d75e3753706ac92fb81de7fd223d70e2659e
Enrico Ros <enrico.ros@gmail.com>
2019-11-17 00:07:08 -0800

    Use the new model from Fred

--- src/loop-cartman.sh
+++ src/loop-cartman.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+source ../venv-tf15/Scripts/activate
 while :
 do
   echo "Press [CTRL+C] to stop.."
-  python flasky.py --http_host=0.0.0.0 --http_port=1303 --model_name=124M-Cartman-Fredliu0 --sample_size=4 --length=60
+  python flasky.py --http_host=0.0.0.0 --http_port=1303 --model_name=345M-Cartman-Fredliu1 --sample_size=4 --length=60
 done
