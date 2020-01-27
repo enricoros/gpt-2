4f5d689b228a726215879d24a4b0f82e429370c9
Enrico Ros <enrico.ros@gmail.com>
2020-01-26 16:51:51 -0800

    Comment

--- src/loop-cartman.sh
+++ src/loop-cartman.sh
@@ -2,6 +2,7 @@
 source ../venv-tf15/Scripts/activate
 while :
 do
-  echo "Press [CTRL+C] to stop.."
+  echo "Looping a GPU (2x40) FredLiu-774 rest server. Press [CTRL+C] to stop."
   python flasky.py --http_host=0.0.0.0 --http_port=1303 --model_name=774M_southpark_convo --sample_size=2 --length=40
+  sleep 2
 done
